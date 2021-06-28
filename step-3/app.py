80fb5b6ae35bb0f4d5cd5a247f128f25efbce5a7
Shannon Donahue <shannon.sad22@gamil.com>
2021-06-28 17:25:49 -0400

    Grabbing API data to create page of users cards

--- step-3/app.py
+++ step-3/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+import requests
+import json
+
 app = Flask(__name__)
 
+#default to get method unless specifically declared
 @app.route("/")
 def testpage():
-	return render_template("UsersList.html")
+	requestusers = requests.get("https://reqres.in/api/users")
+	userlist = json.loads(requestusers.content)
+	return render_template("UsersList.html", userlist = userlist['data'])
